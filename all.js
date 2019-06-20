e4ab3aeb5b25ac21561e1ed39b8010e1360b3a69
komalshivani <komalshivani2@gmail.com>
2019-06-20 13:55:17 -0400

    added extra parameters

--- all.js
+++ all.js
@@ -1,15 +1,19 @@
-function add(a, b){
-    return a + b;
+function add(a, b,c){
+    return a + b+c;
 }
 
-function sub(a, b){
-    return a - b;
+function sub(a, b, c){
+    return a - b-c;
 }
 
-function mul(a, b){
-    return a * b;
+function mul(a, b,c,d){
+    return a * b *c*d;
 }
 
 function div(a, b){
     return a / b;
+}
+
+function all(a,b,c,d){
+    return a+b-c/d
 }
